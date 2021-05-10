7c0011f2324fb3c4a2107d508a504d62f27e5cda
Victoria Kee <victoriakee95@gmail.com>
2021-05-10 10:28:25 -0500

    Grab inital values off html

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,11 @@
 // ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.queryCommandEnabled('.submit-btn');
+const container = document.queryCommandEnabled('.grocery-container');
+const list = document.queryCommandEnabled('.grocery-list');
+const clearBtn = document.queryCommandEnabled('.clear-btn');
 
 // edit option
 
